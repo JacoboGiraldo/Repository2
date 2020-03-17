e806d00afe9f2ab7e7dfad0c943b3d9c42830b7c
Juan Jacobo Giraldo Rodriguez <juanjacobogiraldorodriguez@Juans-MacBook-Air-2.local>
2020-03-17 14:37:42 -0300

    First Cachita del semestre

---
+++ Repo2P/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Repo2P
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jacobo Giraldo");
+            Console.WriteLine("Hope u have a happy Quarantine");
+        }
+    }
+}
